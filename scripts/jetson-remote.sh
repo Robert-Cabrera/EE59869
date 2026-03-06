16f2d4ac52c7fd690c96e8329fd7f9b16a2ccabd
Robert-Cabrera <rcabrer005@citymail.cuny.edu>
2026-03-05 21:29:29 -0500

    Change the connection script to work without need of local USB
    - This assumes both the Jetson and the computer are connected to the same network. In this case my phone's hotspot: SSID: "Robert's iPhone"
    
    - This also assumes that avahi-daemon is running in the Jetson to connect via the mDNS: seniordesign.local
    
    - A little finicky but allows us to connect wirelessly and monitor the inferences in real time :)

--- scripts/jetson-remote.sh
+++ scripts/jetson-remote.sh
@@ -3,4 +3,4 @@ printf "==============================================================\n"
 printf "The login is seniordesign and the password is seniordesign\n"
 printf "==============================================================\n"
 sleep 2
-ssh -L 4002:localhost:4000 seniordesign@seniordesign.local
+ssh seniordesign@seniordesign.local
